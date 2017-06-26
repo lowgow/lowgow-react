2da9d3f3797099316624b656a5ff8b7f862e5be5
Ben Celebicic <ben@hinge.co>
2017-06-26 16:41:55 -0400

    First commit for sample React app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to lowgow</h2>
+        </div>
+        <p className="App-intro">
+          Welcome to logow!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+    
